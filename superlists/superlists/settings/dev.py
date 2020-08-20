1518c90a1a1bdbc25ad8b2cfc38cfd58f7987c47
Mohammad Ayub <mohammad.ayub@imagination.com>
2020-08-20 21:48:45 +0100

    setup the project and wrote the first test

---
+++ superlists/superlists/settings/dev.py
@@ -0,0 +1,17 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable('PGSQL_DB_NAME'),
+        'USER': get_env_variable('PGSQL_DB_USER'),
+        'PASSWORD': get_env_variable('PGSQL_DB_PASW'),
+        'HOST': get_env_variable('PGSQL_DB_HOST'),
+        'PORT': get_env_variable('PGSQL_DB_PORT'),
+        #'OPTIONS': {'sslmode': 'verify-full'},
+    }
+}
