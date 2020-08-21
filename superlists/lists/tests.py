9648a32e9baed8372fe7d10420269780805a1c18
Mohammad Ayub <mohammad.ayub@imagination.com>
2020-08-21 17:27:18 +0100

    added another test

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 
@@ -13,3 +14,11 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEquals(found.func.__name__,
                           HomePageView.as_view().__name__)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = HomePageView.as_view()(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
