61625b38030334448ef80a32b510004d1b6c6ff0
Mohammad Ayub <mohammad.ayub@imagination.com>
2020-08-21 17:16:05 +0100

    first unit test and url mapping, dummy view

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import resolve
 
-# Create your tests here.
+from .views import HomePageView
+
+
+class HomePageTest(TestCase):
+    """
+    Class to test the HomePageView
+    """
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEquals(found.func.__name__,
+                          HomePageView.as_view().__name__)
