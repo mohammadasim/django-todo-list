a283f3f0211f171cee472f12d8f67d3584f84a05
Mohammad Ayub <mohammad.ayub@imagination.com>
2020-10-20 08:05:39 +0100

    fully passing FTs, login, logout working

--- superlists/accounts/urls.py
+++ superlists/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from django.contrib.auth.views import LogoutView
 from .views import send_login_email, login
 urlpatterns = [
     path('send_login_email', send_login_email, name='login_email'),
     path('login', login, name='login'),
+    path('logout', LogoutView.as_view(next_page='/'),  name='logout')
 ]
