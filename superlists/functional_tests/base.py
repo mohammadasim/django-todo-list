e05d5d734cad10d88f817851af3567bfe6479cf5
Mohammad Ayub <mohammad.ayub@imagination.com>
2020-10-21 08:23:13 +0100

    created wait decorator to put wait logic in one place.

--- superlists/functional_tests/base.py
+++ superlists/functional_tests/base.py
@@ -8,6 +8,43 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 
+# The maximum amount of time we are prepared to wait
+Max_Wait = 10
+
+
+def wait(fn):
+    """
+    Decorator function that will apply the wait
+    logic to all the function in its argument.
+    A decorator takes a function as an argument
+    and modifies it.
+    It is important that we add *args **kwargs
+    as this allows the function that is to be
+    decorated to have any number of args and
+    kwargs.
+    We have set a constant for max time.
+    We then have an infinite loop with three
+    ways to exit
+    Either the elements are found, the assertion
+    passes and we exit.
+    Or we raise exception check that we haven't
+    hit our time limit, if not we wait again by
+    calling the sleep.
+    If we hit our time limit we then raise an exception.
+    """
+
+    def modified_fun(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > Max_Wait:
+                    raise e
+                time.sleep(0.5)
+
+    return modified_fun
+
 
 class FunctionalTest(StaticLiveServerTestCase):
     """
@@ -17,8 +54,6 @@ class FunctionalTest(StaticLiveServerTestCase):
     with StaticLiveServerTestCase so static
     files are loaded.
     """
-    # The maximum amount of time we are prepared to wait
-    Max_Wait = 10
 
     def setUp(self) -> None:
         """
@@ -37,34 +72,20 @@ class FunctionalTest(StaticLiveServerTestCase):
         """
         self.browser.quit()
 
+    @wait
     def wait_for_row_in_list_table(self, row_text):
         """
         Method to wait and check for items in a
         table row
-        We have set a constant for max time.
-        We then have an infinite loop with three
-        ways to exit
-        Either the elements are found, the assertion
-        passes and we exit.
-        Or we raise exception check that we haven't
-        hit our time limit, if not we wait again by
-        calling the sleep.
-        If we hit our time limit we then raise an exception.
         :param row_text:
         :return:
         """
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > self.Max_Wait:
-                    raise e
-                time.sleep(0.5)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    @wait
     def wait_for(self, fn):
         """
         Method to cause explicit wait.
@@ -74,18 +95,14 @@ class FunctionalTest(StaticLiveServerTestCase):
         The method takes a function and
         we return the return value of the
         function.
+        The function is a lambda function
+        that checks to find an element in
+        the page.
         as an argument
         :param fn:
         :return:
         """
-        start_time = time.time()
-        while True:
-            try:
-                return fn()
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > self.Max_Wait:
-                    raise e
-                time.sleep(0.5)
+        return fn()
 
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
