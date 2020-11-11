107cc4f705277c7426e7fc04c9690bfbb11c2c3a
Mohammad Ayub <mohammad.ayub@imagination.com>
2020-11-11 08:46:16 +0000

    passing unit and functional tests

--- superlists/lists/tests/test_forms.py
+++ superlists/lists/tests/test_forms.py
@@ -92,3 +92,11 @@ class NewListFormTest(unittest.TestCase):
             first_item_text='new item text', owner=user
         )
 
+    @patch('lists.forms.List.create_new')
+    def test_save_returns_new_list_object(
+            self, mock_list_create_new):
+        user = Mock(is_authenticated=True)
+        form = NewListForm(data={'text': 'new item text'})
+        form.is_valid()
+        response = form.save(owner=user)
+        self.assertEqual(response, mock_list_create_new.return_value)
